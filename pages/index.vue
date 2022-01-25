0eab8abf973bf025651d0ee004ee94a1089d43c4
Norbert Brett <60483492+NobertBrett@users.noreply.github.com>
2022-01-25 21:12:46 +0000

    latest

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="py-20 px-10 text-center">
-    <Logos class="mb-6" />
-    <Suspense>
-      <PageView />
-      <template #fallback>
-        <div class="opacity-50 italic">
-          <span class="pulse">Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-    <InputEntry />
+    <h1 class="text-6xl text-fuchsia-500">Helllooooo</h1>
+    <p class="text-2xl text-emerald-500">I am a paragraph with emerald colour. </p>
+    <DarkToggle />
   </div>
 </template>
+<script>
+import DarkToggle from "../components/DarkToggle";
+export default {
+  components: {DarkToggle}
+}
+</script>
